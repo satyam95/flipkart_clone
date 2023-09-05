99479c52f44098a627cb5b5f2a15d1a41fb804e3
Satyam Sagar <satyamsagar30@gmail.com>
2023-09-05 20:10:48 +0530

    chore: implemented search functionality

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,15 +1,46 @@
-"use client"
+"use client";
 
+import LoadingSpinner from "@/components/LoadingSpinner";
+import ProductHorizontalCard from "@/components/ProductHorizontalCard";
+import { getProductsBySearch } from "@/hooks/getProductsBySearch";
 import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const Search = () => {
   const search = useSearchParams();
   const searchQuery = search ? search.get("q") : null;
+
+  const { data, isLoading } = getProductsBySearch(searchQuery!);
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
-    <span className="text-xl">
-      Showing results for: <span className="font-semibold">{searchQuery}</span>
-    </span>
+    <main>
+      <div className="max-w-[1440px] w-full mx-auto">
+        <div className="m-3">
+          <div className="max-w-[1024px] mx-auto bg-white shadow-[0_2px_4px_0_rgba(0,0,0,.08)] rounded-sm mb-2 px-4 py-2">
+            <div className="text-lg px-4 py-2">
+              Showing results for: {searchQuery}
+            </div>
+            <div className="text-lg px-4 py-2">
+              {data?.products.map((product) => (
+                <ProductHorizontalCard
+                  key={product.id}
+                  id={product.id}
+                  title={product.title}
+                  price={product.price}
+                  brand={product.brand}
+                  rating={product.rating}
+                  description={product.description}
+                  category={product.category}
+                  thumbnail={product.thumbnail}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
---
+++ src/hooks/getProductsBySearch.ts
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface ProductType {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: any[];
+  }
+  
+  interface ProductsType {
+    products: ProductType[];
+  }
+
+export const getProductsBySearch = (searchTerm: string) => {
+  return useQuery({
+    queryKey: [searchTerm],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://dummyjson.com/products/search?q=${searchTerm}`
+      );
+      return data as ProductsType;
+    },
+  });
+};
